9297218187d26bfe7b45502c052bef5b644f881d
tinovyatkin <tino@vtkn.io>
2017-10-16 00:10:55 -0400

    added support for Heroku

--- index.js
+++ index.js
@@ -2,15 +2,28 @@
 
 const { execFileSync } = require('child_process');
 
-function short() {
-  // git rev-parse --short HEAD
-  const res = execFileSync('git', ['rev-parse', '--short', 'HEAD'], {
-    cwd: __dirname,
-    encoding: 'utf8',
-    timeout: 4000,
-  });
+let shortHash;
 
-  // we should trim everything less numbers and letters
-  return res.replace(/[^0-9a-z]*/gim, '');
+function short() {
+  if (shortHash) return shortHash;
+
+  // Heroku Build stage special env variable that is short hash - https://devcenter.heroku.com/changelog-items/630
+  if ('SOURCE_VERSION' in process.env) {
+    shortHash = process.env.SOURCE_VERSION;
+  } else if ('HEROKU_SLUG_COMMIT' in process.env) {
+    // labs metadata https://devcenter.heroku.com/articles/dyno-metadata
+    shortHash = process.env.HEROKU_SLUG_COMMIT.slice(0, 7);
+  } else {
+    // git rev-parse --short HEAD
+    const res = execFileSync('git', ['rev-parse', '--short', 'HEAD'], {
+      cwd: __dirname,
+      encoding: 'utf8',
+      timeout: 4000,
+    });
+
+    // we should trim everything less numbers and letters
+    shortHash = res.replace(/[^0-9a-z]*/gim, '');
+  }
+  return shortHash;
 }
 exports.short = short;
