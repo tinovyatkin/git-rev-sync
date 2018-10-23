27cb05c22fc81e36f5427df35c7cc78e7cec0aa8
Tino Vyatkin <tino@vtkn.io>
2018-10-23 18:07:59 -0400

    remove console output

--- index.test.js
+++ index.test.js
@@ -3,7 +3,6 @@
 const gitHash = require("./");
 
 test("short", () => {
-  console.log(process.env);
   const short = gitHash.short();
   expect(short).toHaveLength(7);
   expect(short).toMatch(/^[\da-z]{7}$/);
