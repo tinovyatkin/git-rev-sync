cc75d307db4625f2b490e3e4b99b2e0c7cca27fe
Tino Vyatkin <tino@vtkn.io>
2018-10-23 17:51:25 -0400

    output all env

--- index.test.js
+++ index.test.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 
-const gitHash = require('./');
+const gitHash = require("./");
 
-test('short', () => {
+test("short", () => {
+  console.log(process.env);
   const short = gitHash.short();
   expect(short).toHaveLength(7);
   expect(short).toMatch(/^[\da-z]{7}$/);
